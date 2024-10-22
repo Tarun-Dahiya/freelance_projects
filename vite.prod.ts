99c60962c890a037995812ab926cdf60925f36c6
criuRW <criu@randwhitney.com>
2024-10-22 17:07:29 -0400

    added floating-ui with examples | code splitting for prod

---
+++ vite.prod.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          // chunk all node_modules
+          if (id.includes('node_modules')) {
+            const dep = id.toString().split('node_modules/')[1].split('/')[0];
+            return dep;
+          }
+        },
+      },
+    },
+  },
+  base: '/webservices/metronic9/',
+},
+)
